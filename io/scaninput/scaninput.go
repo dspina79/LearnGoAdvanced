873d772f5b7be5ad90c0d8549cf6684e2fb1770c
Dave Spina <dspina79@gmail.com>
2021-05-20 20:25:03 -0400

    Wrote output to file mechanism

--- io/scaninput/scaninput.go
+++ io/scaninput/scaninput.go
@@ -8,14 +8,25 @@ import (
 )
 
 func main() {
-
 	scanner := bufio.NewScanner(os.Stdin)
+	f, err := os.Create("outputString")
 
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
 	for scanner.Scan() {
 		line := scanner.Text()
 		lineLower := strings.ToUpper(line)
+		if line == "exit" {
+			break
+		}
+		f.WriteString(line + "\n")
 		fmt.Println(lineLower)
 	}
+
+	f.Sync()
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
