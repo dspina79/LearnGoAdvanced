f9d2cda67933bd726f9edf82d9618195b2faa61d
Dave Spina <dspina79@gmail.com>
2021-05-24 21:36:23 -0400

    Added add to args

--- cli/arguments/arguments.go
+++ cli/arguments/arguments.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
-func checkArgs(args []string) {
+func printArgs(args []string) {
 	if len(args) > 1 {
 		for _, arg := range args {
 			fmt.Println(arg)
@@ -13,7 +14,32 @@ func checkArgs(args []string) {
 	}
 }
 
+func checkArgs(args []string) bool {
+	return len(args) == 3
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	args := os.Args
-	checkArgs(args)
+	if checkArgs(args) {
+		x, err := strconv.Atoi(args[1])
+		checkError(err)
+		y, err := strconv.Atoi(args[2])
+		checkError(err)
+		result := add(x, y)
+
+		fmt.Printf("%d + %d = %d\n", x, y, result)
+
+	} else {
+		panic("The incorrect number of arguments were provided.")
+	}
 }
