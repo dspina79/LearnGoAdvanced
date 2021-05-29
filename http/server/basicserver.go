88f811cd4fb0a4aeb8f7b19a1e9b955a35b4c040
Dave Spina <dspina79@gmail.com>
2021-05-28 23:26:31 -0400

    Finished basic http server

--- http/server/basicserver.go
+++ http/server/basicserver.go
@@ -8,3 +8,18 @@ import (
 func helloWorld(httpWriter http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(httpWriter, "hello, world!\n")
 }
+
+func printHeaders(httpWriter http.ResponseWriter, req *http.Request) {
+	for name, header := range req.Header {
+		for _, h := range header {
+			fmt.Fprintf(httpWriter, "%v: %s", name, h)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", helloWorld)
+	http.HandleFunc("/headers", printHeaders)
+
+	http.ListenAndServe(":8081", nil)
+}
