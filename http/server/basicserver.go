94107e5e3171950e7b80e98e042e76234a20833e
Dave Spina <dspina79@gmail.com>
2021-05-28 23:33:43 -0400

    Added fibonacci request handler

--- http/server/basicserver.go
+++ http/server/basicserver.go
@@ -9,6 +9,16 @@ func helloWorld(httpWriter http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(httpWriter, "hello, world!\n")
 }
 
+func fib50(httpWriter http.ResponseWriter, req *http.Request) {
+	x, y := 0, 1
+	for i := 0; i < 50; i++ {
+		fmt.Fprintf(httpWriter, fmt.Sprint(i+1)+": "+fmt.Sprint(y)+"\n")
+		tempX := x
+		x = y
+		y += tempX
+	}
+}
+
 func printHeaders(httpWriter http.ResponseWriter, req *http.Request) {
 	for name, header := range req.Header {
 		for _, h := range header {
@@ -20,6 +30,6 @@ func printHeaders(httpWriter http.ResponseWriter, req *http.Request) {
 func main() {
 	http.HandleFunc("/hello", helloWorld)
 	http.HandleFunc("/headers", printHeaders)
-
+	http.HandleFunc("/first50fib", fib50)
 	http.ListenAndServe(":8081", nil)
 }
