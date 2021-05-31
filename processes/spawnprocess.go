65282390d636188caa875699aaf4dc42fa68c6d3
Dave Spina <dspina79@gmail.com>
2021-05-30 21:52:22 -0400

    Added grep example

--- processes/spawnprocess.go
+++ processes/spawnprocess.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os/exec"
 )
 
@@ -24,4 +25,15 @@ func main() {
 	checkError(err, "ls")
 	fmt.Println("$> ls -a -l -h")
 	fmt.Println(string(dirOutput))
+
+	grepCommand := exec.Command("grep", "hello")
+	gIn, _ := grepCommand.StdinPipe()
+	gOut, _ := grepCommand.StdoutPipe()
+
+	grepCommand.Start()
+	gIn.Write([]byte("hello grep\ngoodbye grep\nthere are other things than hello"))
+	gIn.Close()
+	grepBytes, _ := ioutil.ReadAll(gOut)
+	fmt.Println(">grep hello")
+	fmt.Println(string(grepBytes))
 }
